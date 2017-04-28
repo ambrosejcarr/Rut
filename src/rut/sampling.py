d5f3a51fefda2758b327e1938cecd6fe83a0b3be
ajc <ambrosejcarr@gmail.com>
2017-04-28 18:46:47 -0400

    added documentation of side-effects, test for sampler. Not working, memory errors

--- src/rut/sampling.py
+++ src/rut/sampling.py
@@ -13,25 +13,45 @@ import pandas as pd
 # todo | draw infinite samples from a normalized data matrix
 
 
-def round_random(norm_data):
+def round_random():
+    pass
+
+
+def normalize():
+    pass
+
+
+def find_sampling_value():
+    pass
+
+
+def draw_sample(normalized_data, n):
+    """Randomly sample n normalized observations from normalized_data
+    :param np.ndarray normalized_data: normalized observations x features matrix. Note
+      that the sum of the features for each observation in this data determines the
+      total number of times the features are observed. Hence, this parameter cannot
+      be set in this function, but must be set by normalize()
+    :param int n: number of observations to draw from normalized_data
+    :param bool return_indices: if True, indices of sampled cells will also be returned
+
+    :return np.ndarray: integer valued sample: n x features array
     """
-    round a sample probabilistically
+    np.random.seed()
 
-    note: this is the most computationally expensive function in this module
+    idx = np.random.randint(0, normalized_data.shape[0], n)
+    sample = normalized_data[idx, :]
 
-    :param np.ndarray norm_data: input data normalized to a particular observation number
+    p = np.random.sample(normalized_data.shape)
+    sample = np.floor(normalized_data) + (normalized_data % 1 > p).astype(int)
 
-    :return np.ndarray: integer-valued sample from norm_data
-    """
-    # todo somewhere I need to draw from norm_data
-    p = np.random.sample(norm_data.shape)
-    return np.floor(norm_data) + (norm_data % 1 > p).astype(int)
+    return sample
 
 
 class Sampler:
 
-    def __init__(self, data, fmap, freduce, n_iter=50, labels=None, sampling_percentile=10, upsample=False,
-                 is_sorted=False):
+    def __init__(
+            self, data, fmap, freduce, n_iter=50, labels=None, sampling_percentile=10,
+            upsample=False, is_sorted=False):
 
         self.fmap = fmap
         self.freduce = freduce
@@ -41,23 +61,26 @@ class Sampler:
 
         # todo add some sanity checks to the input
         if not len(labels) == data.shape[0]:
-            raise ValueError('number of labels (%d) does not match number of observations (%d).' %
-                             (len(labels), data.shape[0]))
+            raise ValueError(
+                'number of labels (%d) does not match number of observations (%d).' %
+                (len(labels), data.shape[0]))
 
-        # if user passes sorted data, both labels and data must be contiguous; probably want a safety check here
+        # if user passes sorted data, both labels and data must be contiguous; probably
+        # want a safety check here
         self._sorted = is_sorted
-        self._normalized = False
+        self._is_normalized = False
 
         # set labels
-        if labels is None:
+        if labels is None:  # todo add support for multi-index, warn when used
             if isinstance(data, pd.DataFrame):
-                self.labels = np.array(data.index)  # todo add support for multi-index, warn when used
+                self.labels = np.array(data.index)
             else:
-                raise ValueError('labels must be provided, either as the index of data or as a separate array passed '
-                                 'using the labels parameter')
+                raise ValueError(
+                    'labels must be provided, either as the index of data or as a '
+                    'separate array passed using the labels parameter')
         else:
-            try:
-                self.labels = np.array(labels)  # should be minimal overhead, I think does nothing if already an array
+            try:  # should be minimal overhead, I think does nothing if already an array
+                self.labels = np.array(labels)
             except:
                 raise TypeError('labels must be convertible to a numpy.array type')
 
@@ -65,13 +88,18 @@ class Sampler:
         if isinstance(data, pd.DataFrame):
             self._columns = np.array(data.columns)
         else:
-            self._columns = np.arange(data.columns)
+            self._columns = np.arange(data.shape[1])
 
         # store numeric indices of original array
         self.index = np.arange(data.shape[0])
 
         # store data
-        self.data = data.values
+        if isinstance(data, pd.DataFrame):
+            self.data = data.values
+        elif isinstance(data, np.ndarray):
+            self.data = data
+        else:
+            raise ValueError('data must be a np.ndarray or pd.DataFrame object')
 
         # default values
         self._numeric_labels = None
@@ -90,7 +118,9 @@ class Sampler:
         return np.ctypeslib.as_array(self.data)
 
     def __iter__(self):
-        """access to the underlying sampler; ideally one would just call the function using currying.
+        """
+        access to the underlying sampler; ideally one would just call the function using
+        currying.
 
         :return:
         """
@@ -101,7 +131,15 @@ class Sampler:
         raise NotImplementedError
 
     def _get_numeric_labels(self):
-        """create and store a numeric translation of labels, as well as their order in the final data"""
+        """
+        create and store a numeric translation of labels, as well as their order in the
+        final data
+
+        :set np.ndarray self._numerical_label_order: store original category labels in
+          their sorted numerical order
+        :set np.ndarray self._numerical_labels: store labels translated into integer
+          indices
+        """
         self._numerical_label_order = np.unique(self.labels)
         if np.issubdtype(self.labels.dtype, np.integer):
             self._numeric_labels = self.labels
@@ -111,7 +149,17 @@ class Sampler:
 
     # copies data, 2n memory usage
     def _sort(self):
-        """order data by labels into contiguous groups, data and numeric labels are updated in-place"""
+        """
+        sort data by self._numeric_labels. Data and labels are sorted and stored in-place
+
+        :set np.ndarray self.data: store data sorted by numerical labels
+        :set np.ndarray self._numeric_labels: store sorted numerical labels
+        :set np.ndarray self.labels: store original labels in sorted order
+        :set np.ndarray self.index: store original df index in sorted order
+        :set bool self._sorted: set sorted flag to True
+
+        :return None:
+        """
         if self._sorted:
             return
         idx = np.argsort(self._numeric_labels)
@@ -122,15 +170,42 @@ class Sampler:
         self._sorted = True
 
     def _identify_splits(self):
-        """identify break points between classes"""
+        """
+        identify break points between classes based on sorted numeric labels
+
+        :set np.ndarray self._splits: break points between classes, used as argument to
+          np.split.
+
+        :return None:
+        """
+        if not self._sorted:
+            raise RuntimeError(
+                'splits cannot be calculated from unsorted data, run self._sort')
+        if self._numeric_labels is None:
+            raise RuntimeError(
+                'labels must be translated into an integer index before splits can be '
+                'calculated. Run self._get_numeric_labels')
         self._splits = np.where(np.diff(self._numeric_labels))[0] + 1
 
     def _calculate_observation_sizes(self):
+        """calculate observation sizes (number of counts in each observation)
+
+        :set np.ndarray self._observation_sizes: store 1d vector of observation sizes
+        :return None:
+        """
         if not self._sorted:
-            raise RuntimeError('data must be sorted before observation sizes can be calculated')
+            raise RuntimeError('data must be sorted before observation sizes can be '
+                               'calculated, run self._sort')
         self._observation_sizes = self.data.sum(axis=1)
 
     def _calculate_n_observations(self):
+        """
+        calculates the number of observations in the smallest group in self.data. Used to
+        determine the size of samples drawn from data.
+
+        :set int self._n_observations: number of observations in smallest group in labels
+        :return None:
+        """
         self._n_observations = min(d.shape[0] for d in np.split(self.data, self._splits))
 
     # copies data, 2n memory usage (overwrites data with a sharedmem array)
@@ -139,18 +214,21 @@ class Sampler:
 
         :return:
         """
+        if self._is_normalized:
+            return
 
         # find the sampling value
         self._get_numeric_labels()
         self._sort()
         self._calculate_observation_sizes()
+        self._identify_splits()
         self._sampling_value = min(
             np.percentile(g, self.sampling_percentile) for g in np.split(self._observation_sizes, self._splits))
 
         # discard cells and labels that are no longer required
         if not self.upsample:
             keep = self._observation_sizes >= self._sampling_value
-            data = self.data[keep, :]
+            self.data = self.data[keep, :]
             self._numeric_labels = self._numeric_labels[keep]
             self.labels = self.labels[keep]
             self.index = self.index[keep]
@@ -164,14 +242,14 @@ class Sampler:
         self.data = np.ctypeslib.as_ctypes(self.data)
         self.data = sharedctypes.Array(self.data._type_, self.data, lock=False)
 
-        self.is_normalized = True
+        self._is_normalized = True
 
     @staticmethod
     def _process_init(data):
         global shared_data
         shared_data = data
 
-    def run(self):
+    def run(self, processes):
 
         # make sure the data is normalized
         self._normalize()
@@ -180,81 +258,10 @@ class Sampler:
         # func(n_observations)
 
         # note that the original would use self.data, not the numpy wrapper for it.
-        with closing(Pool(initializer=self._process_init, initargs=(self.data,))) as pool:
+        with closing(Pool(
+                initializer=self._process_init, initargs=(self.data,),
+                processes=processes)) as pool:
             results = pool.map(self.fmap, repeat(self._n_observations))
 
         self._results = self.freduce(results)  # this will be tricky, since a bunch of shit is stored inside the class.
         return results
-
-
-def find_sampling_value(group_data, percentile):
-    """identify the sampling value
-
-    :param list group_data: among each group, the row sum at percentile is
-      evaluated, and the minimum value across the groups is the sampling value
-    :param int percentile: the percentile to select for the sampling value
-
-    :return int: the minimum value at percentile across the groups in group_data
-    """
-    return min(np.percentile(g.sum(axis=1), percentile) for g in group_data)
-
-
-def normalize(data, downsample_value, upsample=False, labels=None):
-    """Normalize data such that each observation sums to downsample_value
-
-    This function has an important downstream interaction with _draw_sample. In draw
-    sample, non-integer values are rounded probabilistically, such that integer outputs
-    are produced this has the effect of producing observations with very similar
-    distributions to their down-sampled observations, but with more uncertainty
-    associated with observations that have sparser sampling
-
-    :param np.ndarray data: array containing observations x features
-    :param int downsample_value: value to normalize observation counts to.
-    :param bool upsample: if False, all observations with size < downsample_value are
-      excluded. If True, those observations are upsampled to downsample_value.
-    :param np.ndarray labels: array or series of labels, useful to track which cells
-      are being discarded by exclusion of observations with sums below the downsampling
-      value
-    :return np.ndarray: array containing observations x samples where rows sum to
-      downsample_value
-    """
-    obs_size = data.sum(axis=1)
-    if not upsample:
-        keep = obs_size >= downsample_value
-        data = data[keep, :]
-        if labels is not None:
-            labels = labels[keep]
-    norm = (data * downsample_value) / data.sum(axis=1)[:, np.newaxis]
-    if labels is not None:
-        return norm, labels
-    else:
-        return norm
-
-
-def round_random(sample):
-    """round a sample probabilistically"""
-    p = np.random.sample(sample.shape)
-    return np.floor(sample) + (sample % 1 > p).astype(int)
-
-
-def draw_sample(normalized_data, n, return_indices=False):
-    """Randomly sample n normalized observations from normalized_data
-    :param np.ndarray normalized_data: normalized observations x features matrix. Note
-      that the sum of the features for each observation in this data determines the
-      total number of times the features are observed. Hence, this parameter cannot
-      be set in this function, but must be set by normalize()
-    :param int n: number of observations to draw from normalized_data
-    :param bool return_indices: if True, indices of sampled cells will also be returned
-
-    :return np.ndarray: n x features array
-    """
-    np.random.seed()
-    # select cells
-    idx = np.random.randint(0, normalized_data.shape[0], n)
-    sample = normalized_data[idx, :]
-
-    sample = round_random(sample)
-    if return_indices:
-        return sample, idx
-    else:
-        return sample
--- src/rut/test.py
+++ src/rut/test.py
@@ -1,5 +1,5 @@
-from rut.sampling import find_sampling_value
 from rut.mannwhitneyu import mannwhitneyu
+from rut import sampling
 import unittest
 import nose2
 import numpy as np
@@ -32,6 +32,32 @@ class TestMannWhitneyU(unittest.TestCase):
 
     def test_mannwhitneyu(self):
         mannwhitneyu(self.x1, self.x2)
+
+
+class TestSampler(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.x = (np.repeat(10, 1000) + np.random.rand(1000)).reshape(10, 100)
+        cls.y = (np.repeat(100, 1000) + np.random.rand(1000)).reshape(10, 100)
+        cls.labels = np.concatenate([np.ones(10), np.zeros(10)])
+        cls.data = np.vstack([cls.x, cls.y])
+        print(cls.data.shape)
+
+    def test_sampler(self):
+
+        def fmap_test(n):
+            global shared_data
+            data = np.ctypeslib.as_array(shared_data)
+            sample = sampling.draw_sample(data, n)
+            return sample.sum()
+
+        def freduce_test(results):
+            return sum(results)
+
+        sp = sampling.Sampler(self.data, fmap_test, freduce_test, labels=self.labels)
+        print(sp.run(4))
+
 
 
 if __name__ == '__main__':
